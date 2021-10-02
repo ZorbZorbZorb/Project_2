b8a4a9367714007a1f6bc2bc9c952a1d4685290d
Zorb <thatoneperson8888@gmail.com>
2021-10-02 12:13:50 -0400

    Hooked up main menu buttons. Closes #35

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MainMenuController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class MainMenuController : MonoBehaviour {
+
+    [SerializeField] public Button ButtonContinue;
+    [SerializeField] public Button ButtonNewGame;
+    [SerializeField] public Button ButtonOptions;
+    [SerializeField] public Button ButtonQuit;
+
+    void Start() {
+        ButtonNewGame.onClick.AddListener(NewGame);
+        ButtonQuit.onClick.AddListener(Quit);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void NewGame() {
+        SceneManager.LoadScene(1);
+    }
+    public void Quit() {
+        Application.Quit();
     }
 }
