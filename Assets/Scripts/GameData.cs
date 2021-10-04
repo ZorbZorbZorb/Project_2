4a0ee52893f228fd19fde0ca84e4668b53e2053e
Zorb <thatoneperson8888@gmail.com>
2021-10-03 20:28:19 -0400

    GameData.Import anda GameData.Export written.

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,18 +1,56 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts {
     [Serializable]
     public class GameData {
-        public static void ExportData(string path, GameData data) {
-            throw new NotImplementedException();
+        // https://www.youtube.com/watch?v=XOjd_qU2Ido
+        public static void Export(int saveNumber, GameData data) {
+            string path = Application.persistentDataPath + $"/{saveNumber}.save";
+            FileStream stream;
+            try {
+                stream = new FileStream(path, FileMode.OpenOrCreate);
+            }
+            catch {
+                Debug.LogError($"Failed to open or create a file at '{path}'");
+                return;
+            }
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, data);
+            stream.Close();
+            Debug.Log($"Saved game data to '{path}'");
         }
-        public static GameData ImportData(string path) {
-            throw new NotImplementedException();
+        public static GameData Import(int saveNumber) {
+            string path = Application.persistentDataPath + $"/{saveNumber}.save";
+            FileStream stream;
+            try {
+                stream = new FileStream(path, FileMode.Open);
+            }
+            catch {
+                Debug.LogError($"Failed to open game data file at '{path}'");
+                return null;
+            }
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            try {
+                GameData data = (GameData)formatter.Deserialize(stream);
+                stream.Close();
+                Debug.Log($"Loaded game data from '{path}'");
+                return data;
+            }
+            catch {
+                Debug.LogError($"Failed to deserialize game data from '{path}'");
+                stream.Close();
+                return null;
+            }
         }
 
         public int night;
