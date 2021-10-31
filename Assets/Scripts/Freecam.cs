3bcd75d135244ed35e5301a1c9af2224373a70b5
ZorbZorbZorb <thatoneperson8888@gmail.com>
2021-10-31 13:21:22 -0400

    Work on smooth zooming freecam for #72

--- Assets/Scripts/Freecam.cs
+++ Assets/Scripts/Freecam.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class Freecam : MonoBehaviour {
@@ -11,27 +10,33 @@ public class Freecam : MonoBehaviour {
     public static bool NoPan = false;
     public static bool NoZoom = false;
 
-    // How fast the camera moves across the world
-    public float movementSpeed;
-    public float fastMovementSpeed;
-    public float freeLookSensitivity;
-    // How fast the camera zooms in and out of the world
-    public float zoomSens;
-    //public float fastZoomSens;
-    // Are we in free-look?
+    public Camera Camera { get; private set; }
+    /// <summary>Is camera panning this update?</summary>
     private bool panning = false;
+    /// <summary>Camera pan speed</summary>
+    public float FreeLookSensitivity;
+    /// <summary>Amount to zoom the camera when scrolling</summary>
+    public float ZoomSens;
+    /// <summary>Smooth scrolling speed</summary>
+    public float ZoomLerp;
+    /// <summary>What is the zoom the camera is lerping towords?</summary>
+    private float zoomIntent;
+    private float zoomCurrent;
+    private bool zooming;
 
     public BoxCollider2D CameraBounds;
     private float xMin, xMax, yMin, yMax;
     private float halfCamWidth, halfCamHeight;
 
-    // For debugging without spamming the console
-    public float cameraLeft;
-    public float cameraRight;
-    public float cameraUp;
-    public float cameraDown;
+    // Camera bounds positions
+    private float cameraLeft;
+    private float cameraRight;
+    private float cameraUp;
+    private float cameraDown;
 
     private void Awake() {
+        this.Camera = GetComponent<Camera>();
+
         Center = transform.position;
 
         // Recalculate camera size and overages
@@ -43,7 +48,15 @@ public class Freecam : MonoBehaviour {
         xMax = CameraBounds.bounds.max.x;
         yMin = CameraBounds.bounds.min.y;
         yMax = CameraBounds.bounds.max.y;
-        Destroy(CameraBounds.gameObject);
+
+        if ( Debug.isDebugBuild ) {
+            // Disable raycasts on camera bounds collider so we can still see it in debugging
+            gameObject.layer = 2;
+        }
+        else {
+            // Destroy the collider because it is more performant
+            Destroy(CameraBounds.gameObject);
+        }
     }
 
     void Update() {
@@ -73,12 +86,15 @@ public class Freecam : MonoBehaviour {
             if ( axis != 0 ) {
                 ZoomCamera(axis);
             }
+            else if ( zoomIntent != zoomCurrent ) {
+                ApproachZoom();
+            }
         }
     }
 
     private void PanCamera() {
         // How much to pan?
-        var multiplier = freeLookSensitivity * Time.fixedDeltaTime;
+        var multiplier = FreeLookSensitivity * Time.fixedDeltaTime;
         // The clamp will prevent the camera from 'snapping' when alt-tabbing and disorienting the player
         var deltaX = -Mathf.Clamp(multiplier * Input.GetAxis("Mouse X"), -50, 50);
         var deltaY = -Mathf.Clamp(multiplier * Input.GetAxis("Mouse Y"), -50, 50);
@@ -91,9 +107,31 @@ public class Freecam : MonoBehaviour {
         UpdateCameraPosition(newX, newY);
     }
     private void ZoomCamera(float axis) {
-        //var zoomSensitivity = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) ? fastZoomSens : zoomSens;
-        var newSize = Camera.main.orthographicSize + ( -axis * zoomSens );
-        Camera.main.orthographicSize = Mathf.Clamp(newSize, MaxZoom, MinZoom);
+        zoomIntent = Mathf.Clamp(-axis * ZoomSens, MaxZoom, MinZoom);
+        if (zoomIntent != zoomCurrent) {
+            zooming = true;
+            ApproachZoom();
+        }
+
+        //var newSize = Camera.main.orthographicSize + ( -axis * ZoomSens );
+        //Camera.main.orthographicSize = Mathf.Clamp(newSize, MaxZoom, MinZoom);
+
+        // Recalculate the camera size and overages because the camera size has changed
+        //CalculateCameraSize();
+        //UpdateCameraPosition(transform.position.x, transform.position.y);
+    }
+    private void ApproachZoom() {
+        // Approach the intended zoom amount
+        float difference = ( zoomIntent - zoomCurrent );
+        float zoomAmount = difference * Time.fixedDeltaTime;
+        float newSize = Mathf.Clamp(Camera.main.orthographicSize + zoomAmount, MaxZoom, MinZoom);
+        Camera.main.orthographicSize = newSize;
+
+        // Stop zooming if finished approaching
+        if (newSize == MaxZoom || newSize == MinZoom) {
+            zooming = false;
+            zoomIntent = newSize;
+        }
 
         // Recalculate the camera size and overages because the camera size has changed
         CalculateCameraSize();
@@ -130,20 +168,59 @@ public class Freecam : MonoBehaviour {
     void OnDisable() {
         StopPanning();
     }
-    public void StartPanning() {
+    private void StartPanning() {
         panning = true;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
-    public void StopPanning() {
+    private void StopPanning() {
         panning = false;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
+    /// <summary>
+    /// Unpans the camera, returning it to the position it was when the freecam component was created.
+    /// <para>Uses the current main camera</para>
+    /// </summary>
     public static void UnpanCamera() {
         Camera.main.transform.position = Center;
     }
+    /// <summary>
+    /// Unpans the camera, returning it to the position it was when the freecam component was created.
+    /// </summary>
+    public static void UnpanCamera(Camera camera) {
+        camera.transform.position = Center;
+    }
+    /// <summary>
+    /// Unpans the camera, returning it to the position it was when the freecam component was created.
+    /// </summary>
+    public static void UnpanCamera(Freecam freecam) {
+        freecam.Camera.transform.position = Center;
+    }
+    /// <summary>
+    /// Unzooms the camera to MinZoom
+    /// <para>Uses the current main camera. Must to call GetComponent. Please use <see cref="UnZoomCamera(Freecam)"/> instead.</para>
+    /// </summary>
     public static void UnzoomCamera() {
-        Camera.main.orthographicSize = MinZoom;
+        Camera camera = Camera.main;
+        // There's little way around this one besides a singleton, sorry.
+        camera.GetComponent<Freecam>().zoomIntent = MinZoom;
+        camera.orthographicSize = MinZoom;
+    }
+    /// <summary>
+    /// Unzooms the camera to MinZoom
+    /// <para>Must to call GetComponent. Please use <see cref="UnZoomCamera(Freecam)"/> instead.</para>
+    /// </summary>
+    public static void UnzoomCamera(Camera camera) {
+        // There's little way around this one besides a singleton, sorry.
+        camera.GetComponent<Freecam>().zoomIntent = MinZoom;
+        camera.orthographicSize = MinZoom;
+    }
+    /// <summary>
+    /// Unzooms the camera to MinZoom
+    /// </summary>
+    public static void UnZoomCamera(Freecam camera) {
+        camera.zoomIntent = MinZoom;
+        camera.Camera.orthographicSize = MinZoom;
     }
 }
