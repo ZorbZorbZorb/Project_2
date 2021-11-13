8f2c7af906245160b5f5c5a554acf78a5423e4a6
ZorbZorbZorb <thatoneperson8888@gmail.com>
2021-11-13 12:24:09 -0500

    Fixed urinal graphics zfighting

--- Assets/Scripts/Objects/Urinal.cs
+++ Assets/Scripts/Objects/Urinal.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class Urinal : Relief {
     public override InteractableType IType => InteractableType.Urinal;
     public override Vector3 CustomerPositionF => Sideways 
-        ? transform.position + new Vector3() { x = -40, y = 25, z = 1 } 
+        ? transform.position + new Vector3() { x = -40, y = 25, z = -1.1f } 
         : transform.position + new Vector3() { x = 0, y = -5, z = -1 };
     public override Vector3 CustomerPositionM => Sideways 
-        ? transform.position + new Vector3() { x = -45, y = 15, z = 1 }
+        ? transform.position + new Vector3() { x = -45, y = 15, z = -1.1f }
         : transform.position + new Vector3() { x = 0, y = -10, z = -1 };
     public override ReliefType RType => ReliefType.Urinal;
     public override bool HidesCustomer => false;
