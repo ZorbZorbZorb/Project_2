430e2b0a3b9d9036e4f404521063a492324fa50f
Zorb <thatoneperson8888@gmail.com>
2022-04-09 19:49:45 -0400

    Optimized toilet logic

--- Assets/Scripts/Objects/Toilet.cs
+++ Assets/Scripts/Objects/Toilet.cs
@@ -9,24 +9,49 @@ public class Toilet : Relief {
     public override bool HidesCustomer => true;
     public override string DisplayName => "Toilet";
     public override bool CanBeSoiled => false;
-
     public override bool ChangesCustomerSprite => true;
 
     private bool doorClosed = false;
-    
+    private bool faded = false;
+    private static Color colorFaded = new Color(1f, 1f, 1f, 0.7f);
+    private static Color colorNormal = new Color(1f, 1f, 1f, 1f);
+
     // TODO: This doesn't need to be an on update. I was just lazy.
     private void Update() {
         // Open or close the stall door
-        doorClosed = OccupiedBy != null && OccupiedBy.AtDestination;
-        AltSRenderer.sprite = doorClosed ? Collections.spriteStallClosed : Collections.spriteStallOpened;
-        MainSRenderer.sprite = doorClosed ? Collections.spriteToiletOpen : Collections.spriteToiletClosed;
-    }
-    private void OnMouseOver() {
-        if (OccupiedBy != null) {
-            AltSRenderer.color = new Color(1f, 1f, 1f, 0.7f);
+        if ( OccupiedBy != null) {
+            if ( OccupiedBy.AtDestination && !doorClosed ) {
+                doorClosed = true;
+                AltSRenderer.sprite = Collections.spriteStallClosed;
+                MainSRenderer.sprite = Collections.spriteToiletOpened;
+            }
+            else if (!OccupiedBy.AtDestination && doorClosed) {
+                doorClosed = false;
+                AltSRenderer.sprite = Collections.spriteStallOpened;
+                MainSRenderer.sprite = Collections.spriteToiletClosed;
+            }
+        }
+        else {
+            if (doorClosed) {
+                doorClosed = false;
+                AltSRenderer.sprite = Collections.spriteStallOpened;
+                MainSRenderer.sprite = Collections.spriteToiletClosed;
+            }
+            // Unfade the door 
+            if ( faded ) {
+                AltSRenderer.color = colorNormal;
+                faded = false;
+            }
         }
     }
-    private void OnMouseExit() {
-        AltSRenderer.color = new Color(1f, 1f, 1f, 1f);
+    public void OnMouseOver() {
+        if (OccupiedBy != null) {
+            AltSRenderer.color = colorFaded;
+            faded = true;
+        }
+    }
+    public void OnMouseExit() {
+        AltSRenderer.color = colorNormal;
+        faded = false;
     }
 }
