dd2035bfaee1e2884b03a8716fc5d9ca063fe2a6
Zorb <thatoneperson8888@gmail.com>
2022-04-19 18:52:41 -0400

    Added TakeRandom(int)

--- Assets/Scripts/Helpers/RandomExtensions.cs
+++ Assets/Scripts/Helpers/RandomExtensions.cs
@@ -1,9 +1,33 @@
-﻿namespace Assets.Scripts.Helpers {
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts.Helpers {
     public static class RandomExtensions {
         /// <summary>Randomly returns either true or false</summary>
         public static bool Bool() => UnityEngine.Random.Range(0, 2) == 1;
         public static T Random<T>(this T[] collection) {
             return collection[UnityEngine.Random.Range(0, collection.Length)];
+        }
+        public static T Random<T>( this List<T> collection ) {
+            return collection[UnityEngine.Random.Range(0, collection.Count)];
+        }
+        public static IEnumerable<T> TakeRandom<T>(this List<T> collection, int take) {
+            if (collection.Count <= take) {
+                for ( int i = 0; i < collection.Count; i++ ) {
+                    yield return collection[i];
+                }
+                yield break;
+            }
+            else {
+                for ( int i = 0; i < take; i++ ) {
+                    if (collection.Count == 0) {
+                        break;
+                    }
+                    var item = Random(collection);
+                    collection.Remove(item);
+                    yield return item;
+                }
+                yield break;
+            }
         }
     }
 }
